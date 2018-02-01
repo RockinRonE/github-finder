d230fc108f0407d61d66a88e9fda3a8cda3476be
Ronny Ewanek <ronnye@criticalmass.com>
2018-02-01 10:41:43 -0600

    Add alerts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const github = new Github
+const ui = new UI
 
 const searchUser = document.getElementById('searchUser')
 
@@ -8,12 +9,12 @@ searchUser.addEventListener('keyup', e => {
 		github.getUser(userText)
 			.then(data => {
 				if(data.profile.message === 'Not Found') {
-					// alert
+					ui.showAlert('User not found', 'alert alert-danger')
 				} else {
-					// show profile
+					ui.showProfile(data.profile)
 				}
 			})
 	} else {
-		// Clear profile
+		ui.clearProfile()
 	}
 })
